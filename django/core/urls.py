5321320d72ba80368b4100a920c45a439f0a09ae
Mike Allaway <m.j.allaway@bham.ac.uk>
2024-05-28 12:21:02 +0100

    pages app and related work

--- django/core/urls.py
+++ django/core/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 
-urlpatterns = [
-    # Custom apps
+urlpatterns = i18n_patterns(
+    # General app
     path('', include('general.urls')),
+    # CKEditor file uploads
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     # Django admin
     path('dashboard/', admin.site.urls),
-    # Django Debug Toolbar
+    # Debug Toolbar
     path('__debug__/', include('debug_toolbar.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Pages urls (must appear at bottom)
+    path('', include('pages.urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ django/pages/migrations/0002_initial_data.py
@@ -0,0 +1,115 @@
+from django.db import migrations
+from pages import models
+
+
+def create_pages(apps, schema_editor):
+    """
+    Create Page objects with HTML content
+    """
+
+    # Welcome
+    content = """
+
+<h3>Welcome</h3>
+<p>
+    Here's some content
+</p>
+
+""".strip()
+    models.Page.objects.create(
+        name='Welcome',
+        content_en=content,
+        content_es=content,
+        published=True
+    )
+
+    # About
+    content = """
+
+<h2>About</h2>
+<p>
+    Here's some content
+</p>
+
+""".strip()
+    models.Page.objects.create(
+        name='About',
+        content_en=content,
+        content_es=content,
+        published=True
+    )
+
+    # Team Members
+    content = """
+
+<h2>Team Members</h2>
+<p>
+    Here's some content
+</p>
+
+""".strip()
+    models.Page.objects.create(
+        name='Team Members',
+        content_en=content,
+        content_es=content,
+        published=True
+    )
+
+    # Publications
+    content = """
+
+<h2>Publications</h2>
+<p>
+    Here's some content
+</p>
+
+""".strip()
+    models.Page.objects.create(
+        name='Publications',
+        content_en=content,
+        content_es=content,
+        published=True
+    )
+
+        # Contact Us
+    content = """
+
+<h2>Contact Us</h2>
+<p>
+    Here's some content
+</p>
+
+""".strip()
+    models.Page.objects.create(
+        name='Contact Us',
+        content_en=content,
+        content_es=content,
+        published=True
+    )
+
+    # More Resources
+    content = """
+
+<h2>More Resources</h2>
+<p>
+    Here's some content
+</p>
+
+""".strip()
+    models.Page.objects.create(
+        name='More Resources',
+        content_en=content,
+        content_es=content,
+        published=True
+    )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pages', '0001_initial')
+    ]
+
+    operations = [
+        migrations.RunPython(create_pages),
+    ]
